c30880588a9eb74abe742f5501fb84267464c60e
nvmsaab <technobyte8270@gmail.com>
2022-04-29 17:40:08 +0000

    Initial commit

---
+++ Install.bat
@@ -0,0 +1,19 @@
+@echo off
+
+
+pip install cookiejar
+pip install ssl
+pip install os
+pip install requests
+pip install time
+pip install active_count
+pip install Thread
+pip install Write
+pip install cookiejar
+pip install UserAgent
+pip install DeviceTypes
+pip install Platforms
+pip install ApiDomain
+
+
+pause >nul
